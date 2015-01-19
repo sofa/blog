709ae524f158642af6ac0a73a2404db3cceb1d72
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2015-01-19 14:49:36 +0100

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,18 @@
 
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
 
+# Push source and build repos.
+git push origin dev
+
+# Checkout temporally branch where we can add _site directory
+git checkout -b temp
+
 # Build the project.
 jekyll build
 
 # Add changes to git.
 git add -A
+git add -fA _site
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -15,8 +22,7 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
-git push origin dev
 git branch -D gh-pages
 git subtree split -P _site -b gh-pages
 git push -f origin gh-pages:gh-pages
+git branch -D temp
